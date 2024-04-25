8fad6bcf1c6ec0fa30e6af5edaef29725dfaa632
cliffordEmmanuel <cliffemmanuels@gmail.com>
2024-04-25 20:20:53 +0000

    adds search feature to appbar

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Offline Blogging Client',
+      title: 'Blogging Client',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
       home: BlogPage(),
@@ -24,8 +24,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// Blog page should have a search bar....
 class BlogPage extends StatefulWidget {
-  BlogPage({Key? key}) : super(key: key);
+  const BlogPage({Key? key}) : super(key: key);
 
   @override
   State<BlogPage> createState() => _BlogPageState();
@@ -34,7 +35,6 @@ class BlogPage extends StatefulWidget {
 class _BlogPageState extends State<BlogPage> {
   List<Blog>? createdBlogsList;
   List<Blog>? deletedBlogsList;
-  int _counter = 0;
 
   @override
   void initState() {
@@ -46,35 +46,147 @@ class _BlogPageState extends State<BlogPage> {
     deletedBlogsList?.addAll(deletedBlogs);
   }
 
-  void _incrementCounter() {
-    print("New post created!!");
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    const String appTitle = "Offline Blogging Client";
-
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-       //maybe i'll just have the deleted as a tab
-       body: Container(
-         child: BlogsList(
-               title: "published",
-               blogs: createdBlogsList,
-             ),
-         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: _incrementCounter,
-          tooltip: "Create new post",
-          child: const Icon(Icons.add),
-        ),
+      appBar: AppBar(
+        title: const Text("Posts"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search_outlined),
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: SearchResult(createdBlogsList),
+              );
+            },
+          )
+        ],
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Container(
+          //   padding: const EdgeInsets.only(bottom: 5),
+          //   child:
+          //   const Divider(
+          //     color: Colors.grey,
+          //     height: 1,
+          //     indent: 20,
+          //     endIndent: 20,
+          //   ),
+          // ),
 
-
+          Expanded(
+            child: BlogsList(
+              title: "published",
+              blogs: createdBlogsList,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("Button clicked");
+        },
+        tooltip: "Create new post",
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
+
+// For handling the search results
+class SearchResult extends SearchDelegate {
+  List<Blog>? blogsList;
+
+  SearchResult(this.blogsList);
+
+  // a generic search method
+  List<Blog> searchBlogs(String query) {
+    List<Blog> matchQuery = [];
+    for (var blog in blogsList!) {
+      if (blog.title.toLowerCase().contains(query.toLowerCase()) ||
+          blog.blogBody.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(blog);
+      }
+    }
+    return matchQuery;
+  }
+
+  //dynamically changing the search bar behaviour and look depending on what
+  // the user is doing at the time
+
+  // when the search bar is active change icon button to clear!!...
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  // when the search is active display an arrow widget to allow the user
+  // leave the search interface!!
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  // this returns the search results to be displayed
+  @override
+  Widget buildResults(BuildContext context) {
+    List<Blog> matchQuery = searchBlogs(query);
+    if (matchQuery.isEmpty) {
+      return const Center(
+        child: Text('No matching posts found!'),
+      );
+    }
+    return BlogsList(
+        blogs: matchQuery,
+      );
+
+  }
+
+  // this will return suggestions as the user types.
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<Blog> matchQuery = searchBlogs(query);
+    if (matchQuery.isEmpty) {
+      return const Center(
+        child: Text('No suggested posts found!'),
+      );
+    }
+    return BlogsList(
+      blogs: matchQuery,
+    );
+  }
+}
+
+// Posts Section !!!
+// class FirstSection extends StatelessWidget {
+//   const FirstSection({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: [
+//         Text('Posts'),
+//         IconButton(
+//
+//         )
+//       ],
+//     );
+//   }
+// }
